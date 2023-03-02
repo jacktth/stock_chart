fcfa194d8d37fe4f89149a87adb98a434a316e3b
Jack Tang <jack.tangth@gmail.com>
2023-03-02 17:43:22 +0800

    setup supabase and simple auth function for login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { Session } from "@supabase/supabase-js";
+import React, { useEffect, useState } from "react";
+import { supabase } from "./api/supabaseClient";
+import Auth from "./features/auth/Auth";
 import { Chart } from "./features/chart/Chart";
 
 function App() {
-  return <Chart />;
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session },error }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return !session ?<Auth/> :<Chart /> ;
 }
 
 export default App;
---
+++ frontend/src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_REACT_APP_SUPABASE_URL: string
+    readonly VITE_REACT_APP_SUPABASE_ANON_KEY: string
+    readonly REACT_APP_A: string
+
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
