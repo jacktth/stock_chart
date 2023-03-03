8d67c393d0726c99dd9d972c33983960c580ab6e
Jack Tang <jack.tangth@gmail.com>
2023-03-03 16:57:45 +0800

    add method to handle expired login link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Session } from "@supabase/supabase-js";
+import { AuthError, Session } from "@supabase/supabase-js";
 import React, { useEffect, useState } from "react";
 import { supabase } from "./api/supabaseClient";
 import Auth from "./features/auth/Auth";
@@ -6,13 +6,18 @@ import { Chart } from "./features/chart/Chart";
 
 function App() {
   const [session, setSession] = useState<Session | null>(null)
-
+  const [error, setError] = useState<AuthError  | null>(null)
   useEffect(() => {
+    if(window.location.href.includes("Email+link+is+invalid+or+has+expired")){
+      alert("the email link is invalid or has expired, please enter correct link or sign up again if possible")
+    }
+  
     supabase.auth.getSession().then(({ data: { session },error }) => {
       setSession(session)
+     
     })
 
-    supabase.auth.onAuthStateChange((_event, session) => {
+    supabase.auth.onAuthStateChange((event, session) => {
       setSession(session)
     })
   }, [])
