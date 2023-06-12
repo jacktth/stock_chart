69d822a5ee88b3be40347a8e8b657be249f89668
jack tang <jack.tangth@gmail.com>
2023-06-13 01:28:26 +0800

    change name of env variable

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
     readonly VITE_REACT_APP_SUPABASE_URL: string
     readonly VITE_REACT_APP_SUPABASE_ANON_KEY: string
     readonly VITE_SERVER: string
-    readonly VITE_Vistor_email: string
+    readonly VITE_Vistor_Email: string
     readonly VITE_Vistor_Password: string
 
     // more env variables...
