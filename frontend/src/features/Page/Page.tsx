c601e587a9c2003ea9d5ef171a3f4aed06a107fb
jack tang <jack.tangth@gmail.com>
2023-06-13 16:00:24 +0800

    fixing deployment issues, path not resolved

--- frontend/src/features/Page/Page.tsx
+++ frontend/src/features/Page/Page.tsx
@@ -6,40 +6,36 @@ import { selectTopBar } from "../topBar/topBarSlice";
 import TutorialWindow from "../topBar/TutorialWindow";
 import ListingBar from "../listingBar/ListingBar";
 import Chart from "../chart/Chart";
-import TopBar from "../topBar/topBar";
 import ApiPage from "../api/apiPage";
+import TopBar from "../topBar/topBar";
 
- const Page = ({ session }: { session: Session }) => {
+const Page = ({ session }: { session: Session }) => {
   const globalSelect = useAppSelector(selectPage);
   const globalTutorial = useAppSelector(selectTopBar);
 
   const ChartPage = () => {
     return (
       <div className="flex ">
-        {globalTutorial.startTutorial===true ?<TutorialWindow/> :null}
+        {globalTutorial.startTutorial === true ? <TutorialWindow /> : null}
 
         <div className="w-2/12 ">
           <ListingBar session={session} />
-          
         </div>
         <div style={{ height: "100vh" }} className="w-10/12 ">
-
           <TopBar session={session} />
-          <Chart />
 
+          <Chart />
         </div>
       </div>
     );
   };
-
- 
 
   function renderPage() {
     switch (globalSelect) {
       case "chartPage":
         return ChartPage();
       case "apiPage":
-        return <ApiPage session={session} />
+        return <ApiPage session={session} />;
     }
   }
   return <>{renderPage()}</>;
