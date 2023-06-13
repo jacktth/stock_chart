3a04096e15e0b48f19f382a9cdbb805849cac55e
jack tang <jack.tangth@gmail.com>
2023-06-13 15:48:29 +0800

    fixing path problem

--- frontend/src/features/Page/Page.tsx
+++ frontend/src/features/Page/Page.tsx
@@ -6,8 +6,8 @@ import { selectTopBar } from "../topBar/topBarSlice";
 import TutorialWindow from "../topBar/TutorialWindow";
 import ListingBar from "../listingBar/ListingBar";
 import Chart from "../chart/Chart";
-import ApiPage from "../api/apiPage";
 import TopBar from "../topBar/topBar";
+import ApiPage from "../api/apiPage";
 
  const Page = ({ session }: { session: Session }) => {
   const globalSelect = useAppSelector(selectPage);
